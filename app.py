3d3c215b2ee7023a683138c310126edab3ce574d
Abo-Amer <189163655+AboAmer-ai@users.noreply.github.com>
2026-04-14 12:34:51 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -274,5 +274,8 @@ def subscribe():
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        init_db()
+
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
