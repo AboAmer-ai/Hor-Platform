751a411dfe9f8705bbe93ea8907526d49388b094
Abo-Amer <189163655+AboAmer-ai@users.noreply.github.com>
2026-04-21 01:49:20 +0300

    Add import for Header in email_service.py

--- email_service.py
+++ email_service.py
@@ -3,7 +3,7 @@ import smtplib
 import psycopg2
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-
+from email.header import Header
 
 # =========================
 # GET SUBSCRIBERS
